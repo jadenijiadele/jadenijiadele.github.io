21e17efe87618e43f3bcf9bb5b24be7f1ace386f
jadenijiadele <jadenijiadele@atu.edu>
2024-12-06 18:18:31 -0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+// Get references to the button and menu
+const menuButton = document.getElementById('menu-button');
+const menuLinks = document.getElementById('menu-links');
+
+// Toggle menu visibility on button click
+menuButton.addEventListener('click', () => {
+  if (menuLinks.style.display === 'block') {
+    menuLinks.style.display = 'none'; // Hide menu
+  } else {
+    menuLinks.style.display = 'block'; // Show menu
+  }
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
